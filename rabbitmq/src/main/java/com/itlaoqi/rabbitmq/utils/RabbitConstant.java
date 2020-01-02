bddbfd5ad278c050b4b5ebb4e661ddec6afdf615
wyb01 <zhangsitun@sina.com>
2020-01-02 14:26:02 +0800

    2

--- rabbitmq/src/main/java/com/itlaoqi/rabbitmq/utils/RabbitConstant.java
+++ rabbitmq/src/main/java/com/itlaoqi/rabbitmq/utils/RabbitConstant.java
@@ -1,6 +1,7 @@
 package com.itlaoqi.rabbitmq.utils;
 
 public class RabbitConstant {
+
     public static final String QUEUE_HELLOWORLD = "helloworld";
     public static final String QUEUE_SMS = "sms";
     public static final String EXCHANGE_WEATHER = "weather";
--- rabbitmq/src/main/java/com/itlaoqi/rabbitmq/utils/RabbitUtils.java
+++ rabbitmq/src/main/java/com/itlaoqi/rabbitmq/utils/RabbitUtils.java
@@ -3,19 +3,25 @@ package com.itlaoqi.rabbitmq.utils;
 import com.rabbitmq.client.Connection;
 import com.rabbitmq.client.ConnectionFactory;
 
+/**
+* @Description: 工具类
+* @Author: wyb
+* @Date: 2020-01-02 11:35:53
+*/
 public class RabbitUtils {
+    //ConnectionFactory用于创建MQ的物理连接
     private static ConnectionFactory connectionFactory = new ConnectionFactory();
     static {
-        connectionFactory.setHost("127.0.0.1");
-        connectionFactory.setPort(5672);//5672是RabbitMQ的默认端口号
-        connectionFactory.setUsername("guest");
-        connectionFactory.setPassword("guest");
-        connectionFactory.setVirtualHost("/test");
+        connectionFactory.setHost("120.79.178.18");
+        connectionFactory.setPort(5672);                //5672是RabbitMQ的默认端口号
+        connectionFactory.setUsername("wyb");
+        connectionFactory.setPassword("123456");
+        connectionFactory.setVirtualHost("/test");     //虚拟主机（数据库）
     }
     public static Connection getConnection(){
         Connection conn = null;
         try {
-            conn = connectionFactory.newConnection();
+            conn = connectionFactory.newConnection();  //TCP 物理连接
             return conn;
         } catch (Exception e) {
             throw new RuntimeException(e);

de8b5cae895f082b683bbb68e44768685b2a2bfa
wyb01 <zhangsitun@sina.com>
2020-01-06 19:04:48 +0800

    2

--- springboot-consumer/src/main/java/com/itlaoqi/rabbitmq/springboot/MessageConsumer.java
+++ springboot-consumer/src/main/java/com/itlaoqi/rabbitmq/springboot/MessageConsumer.java
@@ -17,17 +17,8 @@ import java.util.Map;
  */
 @Component
 public class MessageConsumer {
-    //@RabbitListener注解用于声明式定义消息接受的队列与exhcange绑定的信息
-    //在SpringBoot中，消费者这端使用注解获取消息
-    @RabbitListener(
-            bindings = @QueueBinding(
-                    value = @Queue(value="springboot-queue" , durable="true"),
-                    exchange = @Exchange(value = "springboot-exchange" , durable = "true" , type = "topic") ,
-                    key = "#"
-            )
-    )
 
-     /**
+    /**
      * @Description: 用于接收消息的方法 - 这个方法运行后将处于等待的状态，有新的消息进来就会自动触发下面的方法处理消息
      *      @Payload 代表运行时将消息"反序列化"后注入到后面的参数中
      * @param employee:
@@ -37,15 +28,23 @@ public class MessageConsumer {
      * @Author: wyb
      * @Date: 2020-01-06 14:32:57
      */
-    @RabbitHandler //通知SpringBoot下面的方法用于接收消息。
-    public void handleMessage(@Payload Employee employee , Channel channel ,
-                              @Headers Map<String,Object> headers) {
+    //@RabbitListener注解用于声明式定义消息接收的"队列"与"exhcange"绑定的信息
+    //在SpringBoot中，消费者这端使用"注解"获取消息
+    @RabbitListener(
+            bindings = @QueueBinding(
+                    value = @Queue(value="springboot-queue" , durable="true"),
+                    exchange = @Exchange(value = "springboot-exchange" , durable = "true" , type = "topic") ,
+                    key = "#"
+            )
+    )
+    @RabbitHandler  //通知SpringBoot下面的方法用于接收消息
+    public void handleMessage(@Payload Employee employee , Channel channel , @Headers Map<String,Object> headers) {
         System.out.println("=========================================");
         System.out.println("接收到" + employee.getEmpno() + ":" + employee.getName());
-        //所有消息处理后必须进行消息的ack，channel.basicAck()
+
         Long tag = (Long)headers.get(AmqpHeaders.DELIVERY_TAG);
         try {
-            channel.basicAck(tag , false);
+            channel.basicAck(tag , false);  //所有消息处理后必须进行消息的ack，channel.basicAck()
         } catch (IOException e) {
             e.printStackTrace();
         }
